447d1997c1abcb5a3913a4ae2e5a26d0df792e45
ramus <ramussiz29@gmail.com>
2025-05-29 22:24:54 +0100

    fit: start login page front

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 "use client";
-
+import { useState } from "react";
+import {motion} from "framer-motion";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Eye, EyeOff, Github, Mail,  } from "lucide-react";
 
 export default function Home() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <h1 className="text-4xl font-bold">Nokidevis</h1>
+        
     </div>
   );
 }
