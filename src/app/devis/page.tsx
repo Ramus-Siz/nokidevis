50922428514ba55db5d1737b3213aa02ac51e315
Ramus Siz <ramussiz29@gmail.com>
2025-06-05 08:29:44 +0100

    feat: create devis page

--- src/app/devis/page.tsx
+++ src/app/devis/page.tsx
@@ -1,7 +1,17 @@
+import DevisTable from "@/components/devisTable";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
 export default function DevisPage() {
     return (
-        <div>
-            <h1>Devis</h1>
-        </div>
-    );
+            <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min p-8">
+               <Button variant="outline" size="sm" className="flex items-center gap-2 mb-12">
+      <Plus className="w-4 h-4" />
+      Nouveau Devis
+    </Button>
+                    
+                <h1 className="text-xl font-bold mb-4">Mes Devis</h1>
+                <DevisTable />
+            </div>
+        );
 }
