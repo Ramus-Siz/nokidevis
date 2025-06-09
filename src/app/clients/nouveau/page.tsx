189607e4ce3a8bc89248aab5f42dc7e18ac6de92
Ramus Siz <ramussiz29@gmail.com>
2025-06-09 09:10:33 +0100

    feat: ajout d'un bouton retour sur la page nouveau client

--- src/app/clients/nouveau/page.tsx
+++ src/app/clients/nouveau/page.tsx
@@ -8,6 +8,8 @@ import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 import { Save } from "lucide-react"
 import { useRouter } from "next/navigation"
+import Link from "next/link"
+import { ArrowLeft } from "lucide-react"
 
 const clientSchema = z.object({
   name: z.string().min(1, "Nom requis"),
@@ -45,7 +47,16 @@ export default function NouveauClientPage() {
 
   return (
     <div className="bg-muted/40 min-h-[100vh] p-8 rounded-xl md:min-h-min">
+    <div className="flex justify-between ">
       <h1 className="text-2xl font-bold pb-8">Créer un nouveau client</h1>
+      <Link href="/clients">
+          <Button variant="outline" size="sm" className="flex items-center gap-2">
+            <ArrowLeft className="w-4 h-4" />
+            Tous les clients
+          </Button>
+        </Link>
+
+    </div>
 
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 max-w-xl">
         <div>
